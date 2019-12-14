a56496325cd15a489d49b329f8c207a18f4802f2
zafarsyed <zafar.habeeb1989@gmail.com>
2019-12-14 17:26:02 +0100

    Refactoring

--- ui-service/src/app/model/cg-data.ts
+++
@@ -1,9 +0,0 @@
-import {CgPath} from './cg-path';
-import {CgTriple} from './cg-triple';
-
-export class CgData {
-  pathList: CgPath[];
-  graphScore: number;
-  inputTriple: CgTriple;
-  finalJudgement?: boolean;
-}
--- ui-service/src/app/model/cg-path.ts
+++
@@ -1,8 +0,0 @@
-import {CgTriple} from './cg-triple';
-
-export class CgPath {
-  path?: CgTriple[];
-  pathScore: number;
-  pathText: string;
-  id?: number;
-}
