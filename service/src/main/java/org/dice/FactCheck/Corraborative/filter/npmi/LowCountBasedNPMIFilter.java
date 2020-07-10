58aeb8183440a23afa450b63eafe37df8e595764
Abdelmoneim Desouki <31845116+abdoudesouki@users.noreply.github.com>
2020-07-10 18:04:46 +0200

    Update LowCountBasedNPMIFilter.java

--- service/src/main/java/org/dice/FactCheck/Corraborative/filter/npmi/LowCountBasedNPMIFilter.java
+++ service/src/main/java/org/dice/FactCheck/Corraborative/filter/npmi/LowCountBasedNPMIFilter.java
@@ -18,7 +18,7 @@ public class LowCountBasedNPMIFilter implements NPMIFilter {
     @Override
     public boolean npmiIsOk(double npmi, int pathLength, double count_path_Predicate_Occurrence,
             double count_Path_Occurrence, int count_predicate_Occurrence) {
-        // TODO put the filtering here
+        
     	if( count_path_Predicate_Occurrence >= this.pathsMinJ[pathLength-1]) return true;
         return false;
     }
