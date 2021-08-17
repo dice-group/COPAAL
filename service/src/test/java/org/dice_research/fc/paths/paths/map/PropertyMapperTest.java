7776fdf7c87fb6afe0197d9187c1c9f15a05b37e
farshad68 <farshad.afshari68@gmail.com>
2021-08-17 11:15:05 +0200

    remove extra imports

--- service/src/test/java/org/dice_research/fc/paths/paths/map/PropertyMapperTest.java
+++ service/src/test/java/org/dice_research/fc/paths/paths/map/PropertyMapperTest.java
@@ -1,41 +1,18 @@
 package org.dice_research.fc.paths.paths.map;
 
-import org.apache.commons.math3.util.Pair;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.ResourceFactory;
-import org.apache.jena.rdf.model.impl.PropertyImpl;
 import org.dice_research.fc.IBidirectionalMapper;
-import org.dice_research.fc.paths.map.PropertyMapper;
 import org.dice_research.fc.run.Application;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.AllTests;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.List;
 
 
-/*@RunWith(SpringJUnit4ClassRunner.class)*/
-/*@RunWith(SpringRunner.class)
-@ContextConfiguration(classes= org.dice_research.fc.config.Config.class , loader= AnnotationConfigContextLoader.class)
-@SpringBootTest*/
 
-/*@RunWith(SpringRunner.class)
-@SpringBootTest
-@ContextConfiguration(classes = org.dice_research.fc.config.Config.class)*/
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
