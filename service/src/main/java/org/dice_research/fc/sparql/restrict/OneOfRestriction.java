a4dc2fd917fee15826840ef094cfdb710c71c59d
Micha <michael.roeder@uni-paderborn.de>
2021-09-07 17:23:19 +0200

    Added javadoc comments.

--- service/src/main/java/org/dice_research/fc/sparql/restrict/OneOfRestriction.java
+++ service/src/main/java/org/dice_research/fc/sparql/restrict/OneOfRestriction.java
@@ -2,8 +2,18 @@ package org.dice_research.fc.sparql.restrict;
 
 import java.util.Collection;
 
+/**
+ * This restriction represents the {@code VALUES} keyword from SPARQL. It means that a variable has
+ * to have one of the listed values (IRIs).
+ * 
+ * @author Michael R&ouml;der (michael.roeder@uni-paderborn.de)
+ *
+ */
 public class OneOfRestriction implements ITypeRestriction {
-
+  
+  /**
+   * The values that are allowed.
+   */
   protected Collection<String> values;
 
   public OneOfRestriction(Collection<String> values) {
