9fdcb181971b00cc9d8c777d720711099a73e814
Michael Röder <michael.roeder@uni-paderborn.de>
2022-09-20 16:33:17 +0200

    Added a missing class.

---
+++ service/src/main/java/org/dice_research/fc/paths/scorer/count/SPARQLBasedResultStreamingCountRetriever.java
@@ -0,0 +1,110 @@
+package org.dice_research.fc.paths.scorer.count;
+
+import org.aksw.jena_sparql_api.core.QueryExecutionFactory;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.ResultSet;
+import org.dice_research.fc.data.Predicate;
+import org.dice_research.fc.data.QRestrictedPath;
+import org.dice_research.fc.paths.scorer.count.max.MaxCounter;
+import org.dice_research.fc.sparql.path.IPathClauseGenerator;
+import org.dice_research.fc.sparql.path.PropPathBasedPathClauseGenerator;
+import org.dice_research.fc.sparql.restrict.ITypeRestriction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * This class makes use of a SPARQL client to query for results and count them, locally. It
+ * shouldn't be used in production since streaming all results may lead to much higher runtimes than
+ * counting them within the SPARQL endpoint.
+ * 
+ * @author Michael R&ouml;der (michael.roeder@uni-paderborn.de)
+ *
+ */
+@Component
+public class SPARQLBasedResultStreamingCountRetriever extends AbstractSPARQLBasedCountRetriever {
+
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(SPARQLBasedResultStreamingCountRetriever.class);
+
+  protected IPathClauseGenerator pathClauseGenerator;
+
+  @Autowired
+  public SPARQLBasedResultStreamingCountRetriever(QueryExecutionFactory qef,
+      MaxCounter maxCounter) {
+    this(qef, maxCounter, new PropPathBasedPathClauseGenerator());
+  }
+
+  @Autowired
+  public SPARQLBasedResultStreamingCountRetriever(QueryExecutionFactory qef, MaxCounter maxCounter,
+      IPathClauseGenerator pathClauseGenerator) {
+    super(qef, maxCounter);
+    this.pathClauseGenerator = pathClauseGenerator;
+  }
+
+  @Override
+  public long countPredicateInstances(Predicate predicate) {
+    StringBuilder queryBuilder = new StringBuilder();
+    queryBuilder.append("SELECT DISTINCT * WHERE { ?s <");
+    queryBuilder.append(predicate.getProperty().getURI());
+    queryBuilder.append("> ?o . ");
+    predicate.getDomain().addRestrictionToQuery("s", queryBuilder);
+    predicate.getRange().addRestrictionToQuery("o", queryBuilder);
+    queryBuilder.append(" }");
+    return executeCountQuery(queryBuilder);
+  }
+
+  protected long countTypeInstances(ITypeRestriction restriction) {
+    StringBuilder queryBuilder = new StringBuilder();
+    queryBuilder.append("SELECT DISTINCT * WHERE { ");
+    restriction.addRestrictionToQuery("s", queryBuilder);
+    queryBuilder.append(" }");
+    return executeCountQuery(queryBuilder);
+  }
+
+  @Override
+  public long countPathInstances(QRestrictedPath path, ITypeRestriction domainRestriction,
+      ITypeRestriction rangeRestriction) {
+    StringBuilder queryBuilder = new StringBuilder();
+    queryBuilder.append("SELECT DISTINCT * WHERE { ");
+    domainRestriction.addRestrictionToQuery("s", queryBuilder);
+    rangeRestriction.addRestrictionToQuery("o", queryBuilder);
+    pathClauseGenerator.addPath(path, queryBuilder, "s", "o");
+    queryBuilder.append(" }");
+    return executeCountQuery(queryBuilder);
+  }
+
+  @Override
+  public long countCooccurrences(Predicate predicate, QRestrictedPath path) {
+    StringBuilder queryBuilder = new StringBuilder();
+    queryBuilder.append("SELECT DISTINCT * WHERE { ?s <");
+    queryBuilder.append(predicate.getProperty().getURI());
+    queryBuilder.append("> ?o . ");
+    predicate.getDomain().addRestrictionToQuery("s", queryBuilder);
+    predicate.getRange().addRestrictionToQuery("o", queryBuilder);
+    pathClauseGenerator.addPath(path, queryBuilder, "s", "o");
+    queryBuilder.append(" }");
+    return executeCountQuery(queryBuilder);
+  }
+
+  protected long executeCountQuery(StringBuilder queryBuilder) {
+    String query = queryBuilder.toString();
+    long time = System.currentTimeMillis();
+    LOGGER.debug("Starting count query {}", query);
+    try (QueryExecution qe = qef.createQueryExecution(query)) {
+      ResultSet result = qe.execSelect();
+      long count = 0;
+      while (result.hasNext()) {
+        result.next();
+        ++count;
+      }
+      LOGGER.debug("Got a query result ({}) after {}ms.", count, System.currentTimeMillis() - time);
+      return count;
+    } catch (Exception e) {
+      LOGGER.error("Got an exception while running count query \"" + query + "\". Returning 0.", e);
+      return 0L;
+    }
+  }
+
+}
