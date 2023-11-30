03106f1f8a55d06d7ab5f71f70d0a9bdc454a605
farshad68 <farshad.afshari68@gmail.com>
2023-11-30 13:59:28 +0100

    examples are json

--- service/src/main/java/org/dice_research/fc/paths/sampler/QRestrictedPathSampler.java
+++ service/src/main/java/org/dice_research/fc/paths/sampler/QRestrictedPathSampler.java
@@ -10,13 +10,14 @@ import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
 import org.dice_research.fc.data.IPieceOfEvidence;
 import org.dice_research.fc.data.QRestrictedPath;
-import org.dice_research.fc.sparql.filter.IRIFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.*;
 
-import java.util.HashSet;
-import java.util.Set;
 
 public class QRestrictedPathSampler implements IPathSampler{
+    private static final Logger LOGGER = LoggerFactory.getLogger(QRestrictedPathSampler.class);
     protected QueryExecutionFactory qef;
     @Autowired
     public QRestrictedPathSampler(QueryExecutionFactory qef) {
@@ -26,8 +27,74 @@ public class QRestrictedPathSampler implements IPathSampler{
     @Override
     public String getSample(Resource subject, Resource object, IPieceOfEvidence path) {
         String output = getOneSample(subject, object, (QRestrictedPath)path);
-        path.setSample(output);
-        return output;
+        String jsonOutput = convertToJSON(output);
+        //String jsonOutput = output;
+        path.setSample(jsonOutput);
+        return jsonOutput;
+    }
+
+    public static String convertToJSON(String input) {
+        Map<String, String> resultMap = new HashMap<>();
+
+
+
+        String[] mainTokens = input.split("\\) \\(");
+        if(mainTokens.length==1){
+            //just x1
+            mainTokens[0] = mainTokens[0].replace("( ","");
+            mainTokens[0] = mainTokens[0].replace(" )","");
+            mainTokens[0] = mainTokens[0].replace("<","");
+            mainTokens[0] = mainTokens[0].replace(">","");
+            input = mainTokens[0];
+        }else{
+            if(mainTokens.length==2){
+                //x1 and x2
+                mainTokens[0] = mainTokens[0].replace("( ","");
+                mainTokens[0] = mainTokens[0].replace(" )","");
+                mainTokens[0] = mainTokens[0].replace("<","");
+                mainTokens[0] = mainTokens[0].replace(">","");
+
+                mainTokens[1] = mainTokens[1].replace("( ","");
+                mainTokens[1] = mainTokens[1].replace(" )","");
+                mainTokens[1] = mainTokens[1].replace("<","");
+                mainTokens[1] = mainTokens[1].replace(">","");
+
+                // because we will split base d on =
+                input = mainTokens[0]+"="+mainTokens[1].replace(" ?x","?x");
+            }else{
+                LOGGER.error(mainTokens.length+"cn not parse this result "+ input);
+            }
+        }
+
+        // Split the input string by spaces and parentheses
+
+        String[] tokens = input.split("=");
+        if(tokens.length==3){
+            String eeror = "eo";
+        }
+        // Process the tokens to extract key-value pairs
+        for (int i = 0; i < tokens.length; i=i+1) {
+            if (tokens[i].startsWith("?x")) {
+                String key = tokens[i];
+                String value = tokens[i + 1];
+                resultMap.put(key, value);
+            }
+        }
+
+        // Convert the map to a JSON string
+        StringBuilder jsonBuilder = new StringBuilder("{ ");
+        for (Map.Entry<String, String> entry : resultMap.entrySet()) {
+            jsonBuilder.append("\"").append(entry.getKey()).append("\": \"").append(entry.getValue()).append("\", ");
+        }
+
+        // Remove the trailing comma and space
+        if (jsonBuilder.length() > 2) {
+            jsonBuilder.setLength(jsonBuilder.length() - 2);
+        }
+
+        jsonBuilder.append(" }");
+
+        return jsonBuilder.toString();
     }
 
     private String getOneSample(Resource subject, Resource object, QRestrictedPath path) {
